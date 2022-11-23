07d792da2c4cbd8f85d041ebdcb3af333ae2af5c
ishwar6 <iisudrj11@gmail.com>
2022-11-23 14:49:39 +0530

    form update

--- blog/account/views.py
+++ blog/account/views.py
@@ -7,11 +7,6 @@ from django.contrib import messages
 def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="account/register.html", context={"register_form":form})
